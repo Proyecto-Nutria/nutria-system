8a20e45a1f2f75180a583aa58022ef89c86e30da
Hugo Jimenez <hugoduar1@gmail.com>
2020-09-18 07:15:39 -0500

    Add GQL schemas

--- functions/typeDefs/types/index.js
+++ functions/typeDefs/types/index.js
@@ -1,5 +1,15 @@
+const { gql } = require('apollo-server-express')
+
+const fs = require('fs');
+const types = gql`${fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8')}`;
+
+
+/** Legacy */
 const { personType } = require('./personType')
+/** END Legacy */
+
 
 module.exports = {
-  personType
+  personType, 
+  types
 }
