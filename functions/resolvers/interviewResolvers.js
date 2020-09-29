f7626cc7b94e57d6a50bccc39121713063b5715b
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-09-28 20:27:32 -0500

    Create example of how to send mail using sendgrid

--- functions/resolvers/interviewResolvers.js
+++ functions/resolvers/interviewResolvers.js
@@ -1,8 +1,28 @@
 const { SingletonAdmin } = require('../models')
+const sgMail = require('@sendgrid/mail')
+const sendgridCredentials = require('../config/sendgrid-credentials.json')
 
 const interviewResolvers = {
   Mutation: {
     createInterview: (_, { interview }) => {
+      // Example of email using sendgrid
+      sgMail.setApiKey(sendgridCredentials.api_key)
+      const msg = {
+        to: 'reyesfragosoroberto@gmail.com',
+        from: 'proyecto.nutria.escom@gmail.com',
+        subject: 'Nutrimail',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+      }
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+
       const userRef = SingletonAdmin.GetInstance().database().ref('interviews/')
       userRef.push(JSON.parse(JSON.stringify(interview)))
       return 'Inserted Into Database'
